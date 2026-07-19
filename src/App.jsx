e4e53a65efdfa815ed619bfbe02486da07ee3602
popeur14a-cell <popeur14.a@gmail.com>
2026-07-19 12:20:17 +0200

    Ajout des données structurées globales

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
@@ -9,24 +10,83 @@ import CollectionDetail from "./pages/CollectionDetail";
 import Parcours from "./pages/Parcours";
 import Contact from "./pages/Contact";
 
+const SITE_URL = "https://www.benett-peintre.fr";
+const SITE_NAME = "Galerie François Benett";
+
 export default function App() {
+  const structuredData = {
+    "@context": "https://schema.org",
+    "@graph": [
+      {
+        "@type": "WebSite",
+        "@id": `${SITE_URL}/#website`,
+        url: SITE_URL,
+        name: SITE_NAME,
+        description:
+          "Galerie officielle de François Benett, peintre contemporain français.",
+        inLanguage: "fr-FR",
+        publisher: {
+          "@id": `${SITE_URL}/#person`,
+        },
+      },
+      {
+        "@type": "Person",
+        "@id": `${SITE_URL}/#person`,
+        name: "François Benett",
+        jobTitle: "Peintre contemporain",
+        image: `${SITE_URL}/images/portrait.jpg`,
+        url: SITE_URL,
+        sameAs: [
+          "https://www.instagram.com/francois_benett/",
+          "https://www.singulart.com/fr/artiste/fran%C3%A7ois-benett-31295?ref=ts",
+        ],
+        homeLocation: {
+          "@type": "Place",
+          name: "Région nantaise, France",
+        },
+      },
+      {
+        "@type": "Organization",
+        "@id": `${SITE_URL}/#organization`,
+        name: SITE_NAME,
+        url: SITE_URL,
+        logo: `${SITE_URL}/logo192.png`,
+      },
+    ],
+  };
+
   return (
     <>
+      <Helmet>
+        <script type="application/ld+json">
+          {JSON.stringify(structuredData)}
+        </script>
+      </Helmet>
+
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
 
-        <Route path="/collections" element={<Collections />} />
+        <Route
+          path="/collections"
+          element={<Collections />}
+        />
 
         <Route
           path="/collections/:collectionId"
           element={<CollectionDetail />}
         />
 
-        <Route path="/parcours" element={<Parcours />} />
+        <Route
+          path="/parcours"
+          element={<Parcours />}
+        />
 
-        <Route path="/contact" element={<Contact />} />
+        <Route
+          path="/contact"
+          element={<Contact />}
+        />
       </Routes>
 
       <Footer />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(
